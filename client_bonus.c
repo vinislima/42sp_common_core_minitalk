b555e19c9b926d452ed2e2fc5e9c99588eebb486
Vinicius Da Silva Lima <vinda-si@c1r4p1.42sp.org.br>
2025-03-19 21:54:56 -0300

    adicionado bonus

---
+++ client_bonus.c
@@ -0,0 +1,88 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   client_bonus.c                                     :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: vinda-si <vinda-si@student.42sp.org.br>    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/03/12 20:22:27 by vinda-si          #+#    #+#             */
+/*   Updated: 2025/03/19 21:18:20 by vinda-si         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft/libft.h"
+#include <sys/types.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <signal.h>
+
+int	g_bit_control;
+
+void	send_char(char c, pid_t pid)
+{
+	int	bit;
+
+	bit = 8 * sizeof(c) - 1;
+	while (bit >= 0)
+	{
+		if (kill(pid, 0) < 0)
+		{
+			ft_printf("ERROR: can't talk with the pid: %d\n", pid);
+			exit(EXIT_FAILURE);
+		}
+		g_bit_control = 0;
+		if (c & (1 << bit))
+			kill(pid, SIGUSR1);
+		else
+			kill(pid, SIGUSR2);
+		bit--;
+		while (g_bit_control != 1)
+			usleep(10);
+	}
+}
+
+void	send_str(char *str, pid_t pid)
+{
+	int	piece;
+
+	piece = 0;
+	while (str[piece])
+	{
+		send_char(str[piece], pid);
+		piece++;
+	}
+	send_char(0, pid);
+}
+
+void	sig_usr(int sig)
+{
+	if (sig == SIGUSR1)
+		g_bit_control = 1;
+	else if (sig == SIGUSR2)
+	{
+		ft_printf("Message received by server\n");
+		exit(EXIT_SUCCESS);
+	}
+}
+
+int	main(int argc, char **argv)
+{
+	pid_t	pid;
+
+	if (argc != 3)
+	{
+		ft_printf("Usage: ./client <pid> <string>\n");
+		exit(EXIT_FAILURE);
+	}
+	signal(SIGUSR1, &sig_usr);
+	signal(SIGUSR2, &sig_usr);
+	pid = ft_atoi(argv[1]);
+	if (!pid)
+	{
+		ft_printf("%s is invalid like pid\n", argv[1]);
+		exit(EXIT_FAILURE);
+	}
+	send_str(argv[2], pid);
+	while (1)
+		sleep(1);
+}
---
+++ server_bonus.c
@@ -0,0 +1,66 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   server.c                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: vinda-si <vinda-si@student.42sp.org.br>    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/03/12 21:13:04 by vinda-si          #+#    #+#             */
+/*   Updated: 2025/03/17 21:44:14 by vinda-si         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft/libft.h"
+#include <sys/types.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <signal.h>
+
+void	init_sig(int sig, void (*handler)(int, siginfo_t *, void *))
+{
+	struct sigaction	susr;
+
+	susr.sa_sigaction = handler;
+	susr.sa_flags = SA_SIGINFO | SA_RESTART | SA_NODEFER;
+	sigemptyset(&susr.sa_mask);
+	if (sig == SIGUSR1)
+		sigaction(SIGUSR1, &susr, 0);
+	else if (sig == SIGUSR2)
+		sigaction(SIGUSR2, &susr, 0);
+}
+
+void	sig_usr(int sig, siginfo_t *info, void *context)
+{
+	static char	c = 0;
+	static int	bit = -1;
+
+	(void)context;
+	if (kill(info->si_pid, 0) < 0)
+	{
+		ft_printf("ERROR: can't send sig to pid: %d\n", info->si_pid);
+		exit(EXIT_FAILURE);
+	}
+	if (bit < 0 && !c)
+		ft_printf("\nClient say: ");
+	if (bit < 0)
+		bit = 8 * sizeof(c) - 1;
+	if (sig == SIGUSR1)
+		c |= 1 << bit;
+	else if (sig == SIGUSR2)
+		c &= ~(1 << bit);
+	if (!bit && c)
+		ft_putchar_fd(c, 1);
+	else if (!bit && !c)
+		kill(info->si_pid, SIGUSR2);
+	bit--;
+	kill(info->si_pid, SIGUSR1);
+}
+
+int	main(void)
+{
+	init_sig(SIGUSR1, &sig_usr);
+	init_sig(SIGUSR2, &sig_usr);
+	ft_printf("pid: %d", getpid());
+	while (1)
+		sleep(1);
+}
