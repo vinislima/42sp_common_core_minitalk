16b25144425c0dc4db0bc2b814b45c681bd1d41e
Vinícius Lima <vinislima@gmail.com>
2025-03-21 19:21:32 -0300

    Avaliado

--- client.c
+++ client.c
@@ -6,7 +6,7 @@
 /*   By: vinda-si <vinda-si@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/12 20:22:27 by vinda-si          #+#    #+#             */
-/*   Updated: 2025/03/21 00:46:02 by vinda-si         ###   ########.fr       */
+/*   Updated: 2025/03/19 21:17:02 by vinda-si         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,7 +21,7 @@ int	g_bit_control;
 void	send_char(char c, pid_t pid)
 {
 	int	bit;
-	
+
 	bit = 8 * sizeof(c) - 1;
 	while (bit >= 0)
 	{
@@ -46,7 +46,6 @@ void	send_str(char *str, pid_t pid)
 	int	piece;
 
 	piece = 0;
-
 	while (str[piece])
 	{
 		send_char(str[piece], pid);
@@ -66,26 +65,21 @@ void	sig_usr(int sig)
 int	main(int argc, char **argv)
 {
 	pid_t	pid;
-	
+
 	if (argc != 3)
 	{
 		ft_printf("Usage: ./client <pid> <string>\n");
 		exit(EXIT_FAILURE);
 	}
-	
 	signal(SIGUSR1, &sig_usr);
 	signal(SIGUSR2, &sig_usr);
 	pid = ft_atoi(argv[1]);
-	
 	if (!pid)
 	{
 		ft_printf("%s is invalid like pid\n", argv[1]);
 		exit(EXIT_FAILURE);
 	}
-	
 	send_str(argv[2], pid);
-	
 	while (1)
-		// usado o sleep para não sobrecarregar o processador
 		sleep(1);
 }
--- server.c
+++ server.c
@@ -6,7 +6,7 @@
 /*   By: vinda-si <vinda-si@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/12 21:13:04 by vinda-si          #+#    #+#             */
-/*   Updated: 2025/03/21 00:49:16 by vinda-si         ###   ########.fr       */
+/*   Updated: 2025/03/17 21:44:14 by vinda-si         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,9 +19,9 @@
 void	init_sig(int sig, void (*handler)(int, siginfo_t *, void *))
 {
 	struct sigaction	susr;
+
 	susr.sa_sigaction = handler;
 	susr.sa_flags = SA_SIGINFO | SA_RESTART | SA_NODEFER;
-
 	sigemptyset(&susr.sa_mask);
 	if (sig == SIGUSR1)
 		sigaction(SIGUSR1, &susr, 0);
@@ -33,77 +33,34 @@ void	sig_usr(int sig, siginfo_t *info, void *context)
 {
 	static char	c = 0;
 	static int	bit = -1;
-	
+
 	(void)context;
-	
 	if (kill(info->si_pid, 0) < 0)
 	{
 		ft_printf("ERROR: can't send sig to pid: %d\n", info->si_pid);
 		exit(EXIT_FAILURE);
 	}
-	// se o bit for menor que 0 e o caractere for diferente de 0
-	// é impresso o caractere e o bit é reiniciado
 	if (bit < 0 && !c)
 		ft_printf("\nClient say: ");
-	// se o bit for somente menor que 0
-	// o bit é reiniciado
-		if (bit < 0)
-		// o bit é reiniciado para o valor de 7
-		// para começar a receber os bits
+	if (bit < 0)
 		bit = 8 * sizeof(c) - 1;
-	// se o sinal for SIGUSR1
-	// o bit é deslocado para a esquerda
-	// e o caractere é atualizado com o bit
 	if (sig == SIGUSR1)
-		// o bit é deslocado para a esquerda
-		// e o caractere é atualizado com o bit
 		c |= 1 << bit;
-	// se o sinal for SIGUSR2
-	// o bit é deslocado para a esquerda
-	// e o caractere é atualizado com o bit	
 	else if (sig == SIGUSR2)
-		// o bit é deslocado para a esquerda
-		// e o caractere é atualizado com o bit.
-		// aqui é aplicado o NOT bit-a-bit (~) e
-		// AND bit-a-bit (&=) para manipular os bits
 		c &= ~(1 << bit);
-	// o simbolo de exclamação é usado para negar o valor
-	// transformando o bit em um "bool", sendo 0 para verdadeiro e 1 para falso
-	// se o bit for verdadeiro e o caractere for diferente de 0
-	// o caractere é impresso	
 	if (!bit && c)
 		ft_putchar_fd(c, 1);
-	// verifica se o bit e o caractere são nulos / valor de zero
-	// se forem, o sinal SIGUSR2 é enviado para o processo
-	// com o pid recebido dessa forma informando que a mensagem foi recebida
-	// e o processo pode enviar a próxima mensagem
 	else if (!bit && !c)
-		// o sinal SIGUSR2 é enviado para o processo
-		// com o pid recebido
 		kill(info->si_pid, SIGUSR2);
-	// o bit é decrementado
-	// para receber o próximo bit		
 	bit--;
-	// o bit é verificado para saber se foi recebido
-	// se não for recebido, o programa fica em loop
-	// esperando o bit ser recebido
 	kill(info->si_pid, SIGUSR1);
 }
-// função principal do server
+
 int	main(void)
 {
-	// a função init_sig é chamada para inicializar o manipulador de sinais
-	// para o sinal SIGUSR1 e SIGUSR2
 	init_sig(SIGUSR1, &sig_usr);
 	init_sig(SIGUSR2, &sig_usr);
-	// é impresso o pid do processo
 	ft_printf("pid: %d", getpid());
-	// o programa fica em loop
-	// esperando o sinal ser recebido
 	while (1)
-		// a função sleep é usada para suspender a execução do programa
-		// por um determinado número de segundos
-		// nesse caso, o programa é suspenso por um segundo
-		// para não sobrecarregar o processador
 		sleep(1);
 }
